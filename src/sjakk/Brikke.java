26cb88905afee961fd4a25a29054e5fb0d78e0fe
Woldelig <christoffer.wold@gmail.com>
2018-05-10 21:51:09 +0200

    kek

---
+++ src/sjakk/Brikke.java
@@ -0,0 +1,21 @@
+package sjakk;
+
+
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+
+
+public abstract class Brikke extends JPanel implements MouseListener {
+
+	
+
+
+	public Brikke() {
+
+	}
+
+
+
+}
--- src/sjakk/EntryPoint.java
+++ src/sjakk/EntryPoint.java
@@ -6,6 +6,7 @@ public class EntryPoint extends JFrame {
 
 	public static void main(String[] args) {
 		new EntryPoint();
+		 System.out.println(Character.codePointAt("\u2654", 0));
 	}
 
 	public EntryPoint() {
